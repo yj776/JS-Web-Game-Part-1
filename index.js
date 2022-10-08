209d028c58a95e0091abf2bd465fc920077eb564
yj776 <yj776@nyu.edu>
2022-10-08 03:10:45 -0400

    JS-Web-Game-Part-1 Main Completed

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(imgSource, leftPosition, bottomPosition){
+    let imgName = document.createElement('img')
+    imgName.src = 'assets/' + imgSource
+    imgName.style.position = 'fixed'
+    imgName.style.left = leftPosition + 'px'
+    imgName.style.bottom = bottomPosition + 'px'
+    document.body.append(imgName)
+}
+
+newImage('green-character.gif', 100,100)
+newImage('pine-tree.png', 450, 200)
+newImage('tree.png', 200, 300)
+newImage('pillar.png', 350, 100)
+newImage('crate.png', 150, 200)
+newImage('well.png', 500, 425)
+
+function newItem(itemSource,leftPosition,bottomPosition){
+    let itemName = document.createElement('img')
+    itemName.src = 'assets/' + itemSource 
+    itemName.style.position = 'fixed'
+    itemName.style.left = leftPosition + 'px'
+    itemName.style.bottom = bottomPosition + 'px'
+    document.body.append(itemName)
+
+    itemName.addEventListener('dblclick',function(){
+        itemName.remove()
+    })
+}
+
+newItem('sword.png',500,405)
+newItem('sheild.png',200, 500)
+newItem('staff.png',100,400)
+
+// let sword = document.createElement('img')
+// sword.src = 'assets/sword.png'
+// sword.position = 'fixed'
+// sword.left = '500px'
+// sword.bottom = '405px'
+// document.body.append(sword)
+
+// sword.addEventListener('click', function(){
+//     sword.remove()
+// })
+
+//Bonus:
+
+
